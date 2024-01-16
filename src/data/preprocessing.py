8f2eac4ae276ab9b3b87161db14b4cab9a9625d4
Pabloo22 <pablete.arino@gmail.com>
2024-01-16 23:55:13 +0100

    Refactor preprocessing.py for improved readability and maintainability

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,27 +1,33 @@
 import pandas as pd
 from geopy.distance import geodesic
 
-KEEP_COLS = ['id',
-            'name', 
-            'number', 
-            'address', 
-            'latitude', 
-            'longitude', 
-            'total_bases']
+KEEP_COLS = [
+    "id",
+    "name",
+    "number",
+    "address",
+    "latitude",
+    "longitude",
+    "total_bases",
+]
+
 
 def build_stations_dataframe(raw_data: list[dict]) -> pd.DataFrame:
     already_in = []
     stations = pd.DataFrame()
     for data in raw_data:
-        for station in data['stations']:
-            if station['id'] not in already_in:
-                stations = pd.concat([stations, pd.DataFrame(station,
-                                                            index=[0], 
-                                                            columns=KEEP_COLS)], 
-                                                            ignore_index=True)
-                already_in.append(station['id'])
+        for station in data["stations"]:
+            if station["id"] not in already_in:
+                stations = pd.concat(
+                    [
+                        stations,
+                        pd.DataFrame(station, index=[0], columns=KEEP_COLS),
+                    ],
+                    ignore_index=True,
+                )
+                already_in.append(station["id"])
 
-    stations.set_index('id', inplace=True)
+    stations.set_index("id", inplace=True)
     return stations
 
 
@@ -31,13 +37,18 @@ def get_k_closest_stations(
     stations_copy = stations.copy(deep=True)
     target_station = stations_copy.loc[target_station_id]
     stations_copy.drop(target_station_id, inplace=True)
-    stations_copy['distance'] = stations_copy.apply(_calculate_distance, axis=1, args=(target_station,))
-    stations_copy.sort_values(by='distance', inplace=True)
+    stations_copy["distance"] = stations_copy.apply(
+        _calculate_distance, axis=1, args=(target_station,)
+    )
+    stations_copy.sort_values(by="distance", inplace=True)
     return stations_copy.iloc[:k]
 
 
 def _calculate_distance(row, target_station):
-    return geodesic((row['latitude'], row['longitude']), (target_station['latitude'], target_station['longitude'])).km
+    return geodesic(
+        (row["latitude"], row["longitude"]),
+        (target_station["latitude"], target_station["longitude"]),
+    ).km
 
 
 def build_dock_bikes_timeseries_dataframe(
@@ -47,15 +58,14 @@ def build_dock_bikes_timeseries_dataframe(
     data_list = []
 
     for d in data:
-        date = pd.to_datetime(d['_id'])
+        date = pd.to_datetime(d["_id"])
         dock_bikes_dict = {}
-        for station in d['stations']:
-            if station['id'] in station_ids:
-                dock_bikes_dict[station['id']] = station['dock_bikes']
+        for station in d["stations"]:
+            if station["id"] in station_ids:
+                dock_bikes_dict[station["id"]] = station["dock_bikes"]
 
         data_list.append(pd.Series(dock_bikes_dict, name=date))
 
     stations_time_series = pd.concat(data_list, axis=1).T
 
     return stations_time_series
-    
