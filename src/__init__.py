5bc4463999cd7e14299fb046c3c50c16bbe2fcc9
Pabloo22 <pablete.arino@gmail.com>
2024-01-19 13:14:14 +0100

    Add visualization function for autocorrelation

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
 from .config import get_config, Config
+from .visualization import (
+    plot_autocorrelation,
+    plot_stations_in_map,
+)
--- src/visualization.py
+++ src/visualization.py
@@ -1,9 +1,51 @@
 """Contains functions for visualizing time series data."""
 import pandas as pd
 import folium
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
 
 RED_ICON = {"color": "red", "icon": "fa-bicycle", "prefix": "fa"}
 BLUE_ICON = {"color": "blue", "icon": "fa-bicycle", "prefix": "fa"}
+
+
+import pandas as pd
+
+
+def plot_autocorrelation(
+    data: pd.DataFrame | pd.Series, figsize=(12, 6), title_prefix=""
+):
+    series = _extract_series(data)
+
+    plt.figure(figsize=figsize)
+
+    plt.subplot(211)
+    plot_acf(
+        series, ax=plt.gca(), title=f"{title_prefix}Autocorrelation Function"
+    )
+
+    plt.subplot(212)
+    plot_pacf(
+        series,
+        ax=plt.gca(),
+        title=f"{title_prefix}Partial Autocorrelation Function",
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+
+def _extract_series(data: pd.DataFrame | pd.Series) -> pd.Series:
+    if isinstance(data, pd.DataFrame):
+        if len(data.columns) != 1:
+            raise ValueError("DataFrame must have only one column")
+        series = data.iloc[:, 0]
+    elif isinstance(data, pd.Series):
+        series = data
+    else:
+        raise TypeError("Input must be a pandas Series or DataFrame")
+
+    return series
 
 
 def plot_stations_in_map(
