58ed82e29611051a7d8c6e2cb7059e73e7bb3f50
Pabloo22 <pablete.arino@gmail.com>
2024-01-22 00:16:28 +0100

    Add plot_windows function for visualizing training and test windows

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,7 @@ from .visualization import (
     plot_acf,
     plot_pacf,
     plot_stations_in_map,
-    plot_mean_values_by_hour_for_each_month
+    plot_mean_values_by_hour_for_each_month,
+    plot_windows,
 )
+from .evaluation import evaluate_forecaster
